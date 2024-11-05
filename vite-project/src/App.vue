cfac10f9a5663c69520fcce9bdc264cac3416dae
Zag <za12ew44zz_999@hotmail.com>
2024-11-05 21:44:35 +0700

    add random number

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,10 +1,56 @@
 <script setup>
+import { ref } from 'vue'
+import RandomNumber from './components/RandomNumber.vue'
+
+
+const currentPage = ref('home')
+
+const switchPage = (page) => {
+  currentPage.value = page
+}
+
+
 </script>
 
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <h1>SuperSim Web</h1>
-    <h1>SuperSim Web2</h1>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#" @click="switchPage('home')">SuperSim Web</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <!-- Main Navigation -->
+          <li class="nav-item">
+            <a class="nav-link active" href="#" @click="switchPage('home')">Home</a>
+          </li>
+          
+          <!-- More Navigation -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              More
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#" @click="switchPage('home')">Home</a></li>
+              <li><a class="dropdown-item" href="#" @click="switchPage('RandomNumber')">Random Number</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Add this section to display components -->
+  <div class="container mt-3">
+    <div v-if="currentPage === 'home'">
+      <h1>Welcome to Home Page</h1>
+    </div>
+    <RandomNumber v-if="currentPage === 'RandomNumber'" />
   </div>
+
+
+  
+
 </template>
---
+++ vite-project/src/components/RandomNumber.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const randomNumber = ref(0)
+
+const generateRandomNumber = () =>{
+  randomNumber.value = Math.floor(Math.random() * 1000) + 1
+  
+}
+</script>
+
+<template>
+  <div>
+    <h1>Hi how are you?</h1>
+    <h1>Are you ready? for random number</h1>
+    <div class="display-number">{{ randomNumber }}</div>
+  </div>
+  <button class="btn btn-primary" @click="generateRandomNumber">Generate Random Number</button>
+</template>
+
+
+<style>
+.display-number{
+  font-size: 10rem;
+  font-weight: bold;
+}
+</style>
